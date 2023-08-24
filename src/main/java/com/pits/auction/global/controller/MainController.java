43ab23ee8ac4550d6754c920c3dee705292d7968
ghlck <ghlckd5424@gmail.com>
2023-08-24 15:09:28 +0900

    # 구조확인 및 modelmapper 추가

---
+++ src/main/java/com/pits/auction/global/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.pits.auction.global.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+
+    @RequestMapping("/")
+    public String index(Model model){
+
+        model.addAttribute("team", "pits");
+
+        return "index";
+    }
+
+}

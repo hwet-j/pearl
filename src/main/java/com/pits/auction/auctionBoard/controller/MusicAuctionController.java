e63cef19e3b8ad63965a567a44a5b91b57b2afc1
whwnd <whwnd5993@naver.com>
2023-08-28 13:55:20 +0900

    DB 정보 가져와서 테이블로 1차 구현

--- src/main/java/com/pits/auction/auctionBoard/controller/MusicAuctionController.java
+++ src/main/java/com/pits/auction/auctionBoard/controller/MusicAuctionController.java
@@ -1,16 +1,30 @@
 package com.pits.auction.auctionBoard.controller;
 
 
+import com.pits.auction.auctionBoard.entity.MusicAuction;
 import com.pits.auction.auctionBoard.service.MusicAuctionService;
+import com.pits.auction.auctionBoard.service.MusicAuctionServiceImpl;
+import jakarta.servlet.annotation.MultipartConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MusicAuctionController {
 
     private final MusicAuctionService musicAuctionService;
-
+    @RequestMapping("/read")
+    public String list(Model model) {
+        List<MusicAuction> musicAuctions= musicAuctionService.findAll();
+        model.addAttribute("musicAuctions", musicAuctions);
+        return "/auction/read";
+    }
 
 
 }
--- src/main/java/com/pits/auction/auctionBoard/service/MusicAuctionService.java
+++ src/main/java/com/pits/auction/auctionBoard/service/MusicAuctionService.java
@@ -3,5 +3,12 @@ package com.pits.auction.auctionBoard.service;
 import com.pits.auction.auctionBoard.entity.MusicAuction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MusicAuctionService {
+
+    List<MusicAuction> findAll();
+
+    Optional<MusicAuction> findById(long id);
 }

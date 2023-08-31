9e6fff64bdeb33adc83b8b3cbec53bf04984c5a2
verisign_90 <verisign_90@naver.com>
2023-08-31 19:50:32 +0900

    타임리프, 유효성 검사 완료

--- src/main/java/com/pits/auction/auctionBoard/controller/MusicAuctionController.java
+++ src/main/java/com/pits/auction/auctionBoard/controller/MusicAuctionController.java
@@ -1,13 +1,9 @@
 package com.pits.auction.auctionBoard.controller;
 
-
-import com.pits.auction.auctionBoard.dto.MusicAuctionDTO;
 import com.pits.auction.auctionBoard.dto.MusicAuctionDTO2;
 import com.pits.auction.auctionBoard.entity.BiddingPeriod;
 import com.pits.auction.auctionBoard.entity.MusicAuction;
 import com.pits.auction.auctionBoard.entity.MusicGenre;
-import com.pits.auction.auctionBoard.repository.BiddingPeriodRepository;
-import com.pits.auction.auctionBoard.repository.MusicGenreRepository;
 import com.pits.auction.auctionBoard.service.BiddingPeriodService;
 import com.pits.auction.auctionBoard.service.MusicAuctionService;
 import com.pits.auction.auctionBoard.service.MusicGenreService;
@@ -15,21 +11,17 @@ import com.pits.auction.auth.entity.Member;
 import com.pits.auction.auth.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 
-import com.pits.auction.auctionBoard.entity.MusicAuction;
-import com.pits.auction.auctionBoard.service.MusicAuctionService;
-import com.pits.auction.auctionBoard.service.MusicAuctionServiceImpl;
-import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -41,7 +33,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MusicAuctionController {
     private final MusicAuctionService musicAuctionService;
-
     private final MusicGenreService musicGenreService;
     private final BiddingPeriodService biddingPeriodService;
     private final MemberService memberService;
@@ -63,7 +54,27 @@ public class MusicAuctionController {
     }
 
     @PostMapping("/write")
-    public String insertAuction(MusicAuctionDTO2 musicAuctionDTO, HttpServletRequest request) {
+    public String insertAuction(@Valid MusicAuctionDTO2 musicAuctionDTO, Errors errors,
+                                HttpServletRequest request, Model model) {
+        if(errors.hasErrors()) {
+            for (FieldError error : errors.getFieldErrors()) {
+                model.addAttribute(error.getField() + "Error", error.getDefaultMessage());
+            }
+
+            List<MusicGenre> genres = musicGenreService.findAllGenres();
+            model.addAttribute("genres", genres);
+
+            List<BiddingPeriod> biddingPeriods = biddingPeriodService.findAllPeriods();
+            model.addAttribute("biddingPeriods", biddingPeriods);
+
+            model.addAttribute("musicAuctionDTO", musicAuctionDTO); // 이미 전달된 DTO 객체를 사용합니다.
+
+            Member anyMember = memberService.findAnyMember();
+            model.addAttribute("AnyMember", anyMember.getNickname());
+
+            return "auction/write";
+        }
+
         try {
             // MultipartFile로 받은 파일들을 저장하기 위한 로직
             String rootPath = request.getSession().getServletContext().getRealPath("/"); // 실제 경로를 구하는 예시
@@ -74,11 +85,7 @@ public class MusicAuctionController {
 
             File albumMusicFile = new File(basePath + musicAuctionDTO.getAlbumMusic().getOriginalFilename());
             musicAuctionDTO.getAlbumMusic().transferTo(albumMusicFile);
-            System.out.println(musicAuctionDTO.getBiddingPeriod());
-            System.out.println(musicAuctionDTO.getBiddingPeriod());
-            System.out.println(musicAuctionDTO.getBiddingPeriod());
-            System.out.println(musicAuctionDTO.getBiddingPeriod());
-            System.out.println(musicAuctionDTO.getBiddingPeriod());
+
             // 음악 경매 정보를 데이터베이스에 저장하기 위한 서비스 호출
             musicAuctionService.saveMusicAuction(musicAuctionDTO);
 

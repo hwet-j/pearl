d604a7378f613283e1f27c211a7fd41dff028eaf
cjswoghks <cjswoghks@gmail.com>
2023-09-01 20:15:39 +0900

    관리자페이지 시큐리티 적용

--- src/main/java/com/pits/auction/auctionBoard/service/MusicAuctionService.java
+++ src/main/java/com/pits/auction/auctionBoard/service/MusicAuctionService.java
@@ -25,4 +25,7 @@ public interface MusicAuctionService {
     Long findLastBidPriceByNickname(String nickname);
 
     Page<MusicAuction> getMusicByOrderByIdDesc(int page, int size);
+
+
+    MusicAuctionDTO2 findDetailById(Long id);
 }

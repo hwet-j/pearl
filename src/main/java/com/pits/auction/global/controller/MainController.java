189603a9eda8d4a1e97f080942f46bacab46c353
ghlck <ghlckd5424@gmail.com>
2023-09-12 09:32:06 +0900

    경매정보 없을 경우 표시되는 텍스트 변경

--- src/main/java/com/pits/auction/global/controller/MainController.java
+++ src/main/java/com/pits/auction/global/controller/MainController.java
@@ -3,11 +3,11 @@ package com.pits.auction.global.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
 
     @RequestMapping("/main/list")
     public String index(Model model){

0f7e27cd9f586661ef67867b43b6f78b37594b7b
정회창 <81364742+hwet-j@users.noreply.github.com>
2023-11-04 11:57:17 +0900

    # 경로수정

--- src/main/java/com/pits/auction/configuration/ResourceConfiguration.java
+++ src/main/java/com/pits/auction/configuration/ResourceConfiguration.java
@@ -10,11 +10,11 @@ public class ResourceConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///home/opc/asset"); // 로컬 경로 설정
+                .addResourceLocations("file:/home/opc/asset/Auction/Image/");
 
         // 오디오 리소스 핸들러 추가
         registry.addResourceHandler("/audios/**")
-                .addResourceLocations("file:///home/opc/asset");
+                .addResourceLocations("file:/home/opc/asset/Auction/Audio/");
     }
 
 }
